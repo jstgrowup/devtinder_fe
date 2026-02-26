d24421389d7bc2e7c16616204988794e90e084d0
jstgrowup <deysubham999@gmail.com>
2026-02-27 00:03:01 +0530

    tooltip and premium feature for chat

---
+++ components/common/tooltip.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface CommonTooltipProps {
+  content: React.ReactNode;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+}
+
+export function CommonTooltip({
+  content,
+  children,
+  side = "top",
+  align = "center",
+}: CommonTooltipProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          {content}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
--- module/requests/components/action-user-card.tsx
+++ module/requests/components/action-user-card.tsx
@@ -1,7 +1,12 @@
+"use client";
 import { Card, CardContent } from "@/components/ui/card";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { REQUEST_STATUS } from "@/module/feed/types";
+import { Lock, MessageCircle } from "lucide-react";
+import { useAuth } from "@/store/authStore";
+import { useState } from "react";
+import { CommonTooltip } from "@/components/common/tooltip";
 
 interface ActionUserCardProps {
   name: string;
@@ -30,6 +35,7 @@ export function ActionUserCard({
   handleReviewRequest,
   actionsAllowed = true,
 }: ActionUserCardProps) {
+  const [locked, setlocked] = useState(true);
   return (
     <Card className="text-black shadow-lg">
       <CardContent className="flex items-center justify-between p-5">
@@ -48,6 +54,33 @@ export function ActionUserCard({
             <p className="text-sm text-zinc-400">{about}</p>
           </div>
         </div>
+        {!actionsAllowed &&
+          (locked ? (
+            <CommonTooltip
+              content="Subscribe to unlock this feature"
+              side="left"
+            >
+              <Button
+                variant="default"
+                size="lg"
+                className="flex items-center gap-2 rounded-full px-5 font-medium opacity-80 cursor-pointer"
+                onClick={() => console.log("open upgrade modal")}
+              >
+                <Lock className="h-4 w-4 " />
+                <span className="leading-none">Message</span>
+              </Button>
+            </CommonTooltip>
+          ) : (
+            <Button
+              variant="default"
+              size="lg"
+              className="flex items-center gap-2 rounded-full px-5 font-medium transition-colors active:scale-95 cursor-pointer"
+              onClick={() => console.log("chat")}
+            >
+              <MessageCircle className="h-4 w-4 " />
+              <span className="leading-none">Message</span>
+            </Button>
+          ))}
         {actionsAllowed && (
           <div className="flex gap-3">
             <Button
